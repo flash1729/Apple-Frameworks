4e1dfd472a696d2abe155e4a23051260e2664302
Aditya Medhane <adityasmac@Adityas-MacBook-Air-2.local>
2024-06-21 15:08:08 +0530

    Initial commit: Add project files

---
+++ Apple-Frameworks/Apple_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworksApp.swift
+//  Apple-Frameworks
+//
+//  Created by Aditya Medhane on 21/06/24.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
