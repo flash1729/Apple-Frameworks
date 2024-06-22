1f296ac4a5355c78b36ef6cd47c0a67eceff72b0
Aditya Medhane <adityasmac@Adityas-MacBook-Air-2.local>
2024-06-22 10:51:11 +0530

    Completed FrameworkGridView and also added icons related

---
+++ Apple-Frameworks/FramworkGridView.swift
@@ -0,0 +1,18 @@
+//
+//  FramworkGridView.swift
+//  Apple-Frameworks
+//
+//  Created by Aditya Medhane on 21/06/24.
+//
+
+import SwiftUI
+
+struct FramworkGridView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FramworkGridView()
+}
